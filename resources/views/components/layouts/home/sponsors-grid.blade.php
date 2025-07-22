3ee923a1619ebde7f7567064ffc1c7c2d1845cc1
popo0015 <popo0015@hz.nl>
2025-07-22 13:12:18 +0200

    Minor tweeks to the Sponsor part

--- resources/views/components/layouts/home/sponsors-grid.blade.php
+++ resources/views/components/layouts/home/sponsors-grid.blade.php
@@ -1,8 +1,8 @@
 @props(['sponsors'])
 
 <section class="p-20">
-    <h2 class="text-hw-green text-5xl font-title mb-6">Our Partners</h2>
-    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+    <h2 class="text-white text-5xl font-bayon mb-6">Thank you to our Sponsors!</h2>
+    <div class="bg-white p-12 max-w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
         @foreach($sponsors as $sponsor)
             <div class="flex items-center justify-center p-4 bg-white rounded-lg shadow">
                 <img src="{{ asset('storage/' . $sponsor->image_path) }}"
@@ -10,5 +10,9 @@
                      class="max-h-16 object-contain"/>
             </div>
         @endforeach
+        <h1>Waiting for sponsors!</h1>
+
+        <!-- Mascot -->
+        <img src="{{ asset('/img/main_logo_planet.png') }}" alt="Mascot" class="w-[420px] mr-0 h-auto pointer-events-none">
     </div>
 </section>
