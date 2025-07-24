11d328180295ee6f8fa94aaad13e5b4e19b996f1
popo0015 <popo0015@hz.nl>
2025-07-24 12:23:56 +0200

    Fixed some phpstan errors like return types and null coverage of the home function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,10 +6,15 @@ use App\Models\Event;
 use App\Models\SchoolYear;
 use App\Models\Sponsor;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|object
+     * passes the last 3 events, the sponsors, and some information about the join us section
+     */
+    public function index() :View
     {
         $events = Event::latest()->take(3)->get();
         $sponsors = Sponsor::all();
@@ -24,26 +29,30 @@ class HomeController extends Controller
 
         $schoolYear = SchoolYear::latest('start_academic_year')->first();
 
-        $membershipPrices = [
-            [
-                'label' => 'Early bird – ' . $schoolYear->early_membership_price . '€',
-                'highlight' => 'Few left',
-                'style' => 'bg-gray-50',
-                'highlightColor' => 'bg-hw-pink',
-            ],
-            [
-                'label' => 'Normal – ' . $schoolYear->regular_membership_price . '€',
-                'highlight' => null,
-                'style' => 'bg-hw-green',
-                'highlightColor' => null,
-            ],
-            [
-                'label' => '1 semester – ' . $schoolYear->semester_membership_price . '€',
-                'highlight' => null,
-                'style' => 'bg-gray-50',
-                'highlightColor' => null,
-            ],
-        ];
+        $membershipPrices = [];
+
+        if ($schoolYear) {
+            $membershipPrices = [
+                [
+                    'label' => 'Early bird – ' . $schoolYear->early_membership_price . '€',
+                    'highlight' => 'Few left',
+                    'style' => 'bg-white',
+                    'highlightColor' => 'bg-hw-pink',
+                ],
+                [
+                    'label' => 'Normal – ' . $schoolYear->regular_membership_price . '€',
+                    'highlight' => null,
+                    'style' => 'bg-hw-green',
+                    'highlightColor' => null,
+                ],
+                [
+                    'label' => '1 semester – ' . $schoolYear->semester_membership_price . '€',
+                    'highlight' => null,
+                    'style' => 'bg-white',
+                    'highlightColor' => null,
+                ],
+            ];
+        }
 
         return view('welcome', compact('events', 'sponsors', 'memberBenefits', 'membershipPrices'));
     }
