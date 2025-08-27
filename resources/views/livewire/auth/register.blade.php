aad4b115a15b2c8f1f55e2248cdcea6a90d4734b
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-08-27 10:52:12 +0200

    Redesign the register and add school year to it

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,62 +1,84 @@
+@php use App\Enums\StudyYear; @endphp
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
-
+    <div class="text-center">
+        <h1 class="text-2xl font-bayon text-white">Create an account</h1>
+        <p class="text-sm text-hw-blue-400 font-handwriting">Enter your details below to create your account</p>
+    </div>
     <!-- Session Status -->
-    <x-auth-session-status class="text-center" :status="session('status')" />
+    <x-auth-session-status class="text-center" :status="session('status')"/>
 
     <form wire:submit="register" class="flex flex-col gap-6">
-        <!-- Name -->
-        <flux:input
-            wire:model="name"
-            :label="__('Name')"
-            type="text"
-            required
-            autofocus
-            autocomplete="name"
-            :placeholder="__('Full name')"
-        />
+        <div>
+            <x-hw.label for="name" value="Name"/>
+            <x-hw.input
+                wire:model="name"
+                type="text"
+                required
+                autofocus
+                autocomplete="name"
+                placeholder="Full name"
+            />
+            @error('name') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
+        </div>
 
-        <!-- Email Address -->
-        <flux:input
-            wire:model="email"
-            :label="__('Email address')"
-            type="email"
-            required
-            autocomplete="email"
-            placeholder="email@example.com"
-        />
+        <div>
+            <x-hw.label for="email" value="Email"/>
+            <x-hw.input
+                wire:model="email"
+                type="email"
+                required
+                autocomplete="email"
+                placeholder="mail0000@hz.nl"
+            />
+            @error('email') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
+        </div>
 
-        <!-- Password -->
-        <flux:input
-            wire:model="password"
-            :label="__('Password')"
-            type="password"
-            required
-            autocomplete="new-password"
-            :placeholder="__('Password')"
-            viewable
-        />
+        <div>
+            <x-hw.label for="year" value="Study Year"/>
+            <select name="year" id="year" required wire:input="study_year"
+                    class="w-full p-2 border-hw-blue border bg-hw-dark text-gray-200 focus:border-hw-blue-600 focus:ring-hw-blue-600 rounded shadow-xs">
+                @foreach(StudyYear::cases() as $year)
+                    <option
+                        value="{{ $year->value }}">
+                        {{ ucfirst($year->value) }}
+                    </option>
+                @endforeach
+            </select>
+            @error('study_year') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
+        </div>
 
-        <!-- Confirm Password -->
-        <flux:input
-            wire:model="password_confirmation"
-            :label="__('Confirm password')"
-            type="password"
-            required
-            autocomplete="new-password"
-            :placeholder="__('Confirm password')"
-            viewable
-        />
+        <div>
+            <x-hw.label for="password" value="Password"/>
+            <x-hw.input
+                wire:model="password"
+                type="password"
+                required
+                autocomplete="new-password"
+            />
+            @error('password') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
+        </div>
+
+        <div>
+            <x-hw.label for="password" value="New Password"/>
+            <x-hw.input
+                wire:model="password_confirmation"
+                type="password"
+                required
+                autocomplete="new-password"
+            />
+            @error('password') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
+        </div>
 
         <div class="flex items-center justify-end">
-            <flux:button type="submit" variant="primary" class="w-full">
-                {{ __('Create account') }}
-            </flux:button>
+            <button type="submit"
+                    class="w-full bg-hw-blue-600 hover:bg-hw-blue-800 hover:cursor-pointer text-white font-medium py-3 px-8 rounded-lg transition-colors duration-200">
+                Create account
+            </button>
         </div>
     </form>
 
-    <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
+    <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-400">
         <span>{{ __('Already have an account?') }}</span>
-        <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
+        <a href="route('login')" wire:navigate class="underline text-hw-blue-300">{{ __('Log in') }}</a>
     </div>
 </div>
